71591cade836c7d0209e42d776203a9c1b8e47d9
Silas Rioux <silas.rioux@gmail.com>
2017-01-11 19:33:06 -0700

    commit docs

---
+++ docs/api_project.js
@@ -0,0 +1,14 @@
+define({
+  "name": "Anagram",
+  "version": "0.0.0",
+  "description": "Playing around with Node.js",
+  "url": "localhost:3011",
+  "sampleUrl": false,
+  "apidoc": "0.2.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2017-01-12T02:30:35.960Z",
+    "url": "http://apidocjs.com",
+    "version": "0.11.0"
+  }
+});
